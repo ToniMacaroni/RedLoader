fb0fb2ad2809dd0f8ec8ff0049812850a55d8c2c
Julian Kittel <62687014+ToniMacaroni@users.noreply.github.com>
2023-11-22 20:08:42 +0100

    assembly type support for manifest.json

--- SonsSdk/ManifestData.cs
+++ SonsSdk/ManifestData.cs
@@ -10,42 +10,42 @@ public class ManifestData
     [DecodeAlias("id")]
     [Include]
     public string Id { get; internal set; }
-    
+
     /// <summary>
     /// Name of the mod
     /// </summary>
     [DecodeAlias("name")]
     [Include]
     public string Name { get; internal set; }
-    
+
     /// <summary>
     /// Author of the mod
     /// </summary>
     [DecodeAlias("author")]
     [Include]
     public string Author { get; internal set; }
-    
+
     /// <summary>
     /// Version of the mod
     /// </summary>
     [DecodeAlias("version")]
     [Include]
     public string Version { get; internal set; }
-    
+
     /// <summary>
     /// Description of the mod
     /// </summary>
     [DecodeAlias("description")]
     [Include]
     public string Description { get; internal set; }
-    
+
     /// <summary>
     /// Game version the mod is compatible with
     /// </summary>
     [DecodeAlias("gameVersion")]
     [Include]
     public string GameVersion { get; internal set; }
-    
+
     /// <summary>
     /// Loader version the mod is compatible with
     /// </summary>
@@ -59,14 +59,14 @@ public class ManifestData
     [DecodeAlias("platform")]
     [Include]
     public string Platform { get; internal set; } = "Client";
-    
+
     /// <summary>
     /// Optional. List of dependencies of the mod
     /// </summary>
     [DecodeAlias("dependencies")]
     [Include]
     public string[] Dependencies { get; internal set; }
-    
+
     /// <summary>
     /// Optional. The hex string color in which the mod's name will be displayed in the console
     /// </summary>
@@ -81,13 +81,20 @@ public class ManifestData
     [DecodeAlias("url")]
     [Include]
     public string Url { get; internal set; }
-    
+
     /// <summary>
     /// Optional. Priority of the mod.
     /// </summary>
     [DecodeAlias("priority")]
     [Include]
     public int Priority { get; internal set; }
-    
+
+    /// <summary>
+    /// Type of the assembly. Possible values: "Mod", "Library".
+    /// </summary>
+    [DecodeAlias("type")]
+    [Include]
+    public string Type { get; internal set; } = "Mod";
+
     public Version VersionObject { get; internal set; }
 }
